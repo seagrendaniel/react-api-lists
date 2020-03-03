6eb3552d9e04883841597b417f7ac4f8cad89fbe
blackdanamite <seagrendaniel562@gmail.com>
2020-03-02 23:25:24 -0800

    first commit and SearchBar component added

---
+++ src/Components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// import ImageList from './Components/ImageList';
+import SearchBar from './SearchBar';
+
+const App = () => {
+  return(
+    <div className="ui container" style={{marginTop: '10px'}}>
+    <SearchBar/>
+    </div>
+  );
+}
+
+export default App;
