e54e739dfb00ee77bc7b332fd3191130fda5a9d3
blackdanamite <seagrendaniel562@gmail.com>
2020-03-10 14:40:44 -0700

    add api folder and create custom axios client for unsplash api

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -4,13 +4,20 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    console.log(term);
+  state = { images: [] };
+
+  onSearchSubmit = async (term) => {
+    const response = await axios.get('/search/photos', {
+      params: { query: term },
+    });
+
+    this.setState({images: response.data.results});
   }
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
       </div>
     );
   }
