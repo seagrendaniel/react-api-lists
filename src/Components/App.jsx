6c6a8359e6b7a7e91967ca1f1f5141525be75dac
blackdanamite <seagrendaniel562@gmail.com>
2020-03-10 13:53:41 -0700

    add axios to application to allow API requests

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
+import axios from 'axios';
 // import ImageList from './Components/ImageList';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-
-  
+  onSearchSubmit(term) {
+    console.log(term);
+  }
   render() {
-  return(
-    <div className="ui container" style={{marginTop: '10px'}}>
-    <SearchBar/>
-    </div>
-  );
+    return (
+      <div className="ui container" style={{ marginTop: '10px' }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    );
   }
 }
 
