19b452b2621edac69c2b4244498f81b13ee44fd6
blackdanamite <seagrendaniel562@gmail.com>
2020-03-10 14:54:56 -0700

    create ImageList component and render images based on search term

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
-import axios from 'axios';
-// import ImageList from './Components/ImageList';
+import unsplash from '../api/unsplash';
+import ImageList from './ImageList';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
   state = { images: [] };
 
   onSearchSubmit = async (term) => {
-    const response = await axios.get('/search/photos', {
+    const response = await unsplash.get('/search/photos', {
       params: { query: term },
     });
 
-    this.setState({images: response.data.results});
+    this.setState({ images: response.data.results });
   }
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
-        Found: {this.state.images.length} images
+        <ImageList images={this.state.images} />
       </div>
     );
   }
